84f9f978d04fdc270097a3b96a8def4e27d6f93e
Hafiz Darmawan <hafizdarmawan1996@gmail.com>
2024-07-10 15:23:03 +0700

    perbaikan import echo-jwt
    penambahan v4

--- internal/routes/route.go
+++ internal/routes/route.go
@@ -6,8 +6,8 @@ import (
 	comments "newsapps/internal/features/comments"
 	users "newsapps/internal/features/users"
 
-	"github.com/golang-jwt/jwt"
-	echojwt "github.com/labstack/echo-jwt"
+	"github.com/golang-jwt/jwt/v5"
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
